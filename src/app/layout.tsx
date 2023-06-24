1f5dc9321b6e6b5b4ae7798eab81c30c2d9d8e83
Kleuber Silva <kleuber20@hotmail.com>
2023-06-24 12:59:04 -0300

    Modified files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
 
   return (
     <html lang="en">
+      <header>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </header>
       <body className={inter.className}>
         <Provider>
           <NavBar />
